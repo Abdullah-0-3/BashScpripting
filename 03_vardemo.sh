74887fcb0ea66485d51b2148f0116fd37e4fbb97
Muhammad Abdullah Abrar <abdullahabrar4843@gmail.com>
2024-11-24 10:23:35 +0500

    03_vardemo.sh --- 24 Nov 2024

---
+++ 03_vardemo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script to Use Variables
+
+name="Abdullah"
+age=16
+
+echo "Ok! You are $name and your $age years old..'"
+echo "I already knew that!"
+
+# Variable to Store Command Output
+
+hostname=$(hostname)
+echo "Your machine is $hostname"
